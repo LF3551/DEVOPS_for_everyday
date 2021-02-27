ca82e7425d78f2b884f74de3925199a3582ab937
Aleksey <63855894+LF3551@users.noreply.github.com>
2021-02-27 23:23:32 +0300

    Create songs_translate.py

---
+++ songs_translate.py
@@ -0,0 +1,29 @@
+import requests
+from lxml import etree
+import lxml.html
+import csv
+
+
+'''
+parce songs from amalgamma with translations
+'''
+
+def parce(url):
+    try:
+        api = requests.get(url)
+    except:
+        return
+    tree = lxml.html.document_fromstring(api.text)
+    text_original = tree.xpath('//*[@id="click_area"]/div//*[@class="original"]/text()')
+    text_translate = tree.xpath('//*[@id="click_area"]/div//*[@class="translate"]/text()')
+    with open("text.csv", "w", newline = '') as csv_file:
+        write = csv.writer(csv_file)
+        for i in range(len(text_original)):
+            print(text_original[i], text_translate[i])
+            write.writerow(text_original[i])
+            write.writerow(text_translate[i])
+def main(url):
+    parce(url)
+
+if __name__ == "__main__":
+    main("https://www.amalgama-lab.com/songs/r/robbie_williams/feel.html")
